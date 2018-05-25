2fe7f137150e85b4fa8b44f375d24ac26a7c9005
Victor Henrique <vhenrique21@icloud.com>
2018-05-25 01:29:19 -0300

    Valor 6

--- Cliente.java
+++ Cliente.java
@@ -3,7 +3,7 @@ public class Cliente implements Autenticavel {
 
 	private AutenticacaoUtil autenticador;
 	private int teste = 15;
-	private int valor = 2;
+	private int valor = 6;
 	
 	public Cliente() {
 		this.autenticador = new AutenticacaoUtil();
